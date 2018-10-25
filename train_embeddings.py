86e79fea754bd7032eac8153760fb04401211f24
bensarthou <bsarthou@hotmail.fr>
2018-10-25 11:41:33 +0200

    [+] Adding prediction and precision computation on words embedding

--- train_embeddings.py
+++ train_embeddings.py
@@ -15,9 +15,9 @@ EPISODES_TEST = [9, 10, 11, 12]
 PERSONS = ['howard_wolowitz', 'sheldon_cooper', 'leonard_hofstadter', 'penny',
            'rajesh_koothrappali']
 GLOVE_DIR = 'data/'
-EMBEDDING_DIM = 100
+EMBEDDING_DIM = 300
 
-sentences, labels = load_sentences_persons(EPISODES_LEARN, states=PERSONS)
+sentences, labels, _, _ = load_sentences_persons(EPISODES_LEARN, states=PERSONS)
 
 maxlen = 500  # We will cut sentence after 461 words (max is 461))
 max_words = 10000  # We will only consider the top 10,000 words in the dataset
@@ -56,7 +56,7 @@ y_val = labels[round(0.8*n_samples):]
 # Loading pre-embedding data
 embeddings_index = {}
 # WARNING watch the embedding dim
-f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.'+str(EMBEDDING_DIM)+'d.txt'))
 for line in f:
     values = line.split()
     word = values[0]
@@ -79,12 +79,12 @@ for word, i in word_index.items():
 model = Sequential()
 model.add(Embedding(max_words, EMBEDDING_DIM, input_length=maxlen))
 model.add(Flatten())
-#model.add(Dense(10000, activation='relu'))
-#model.add(Dense(5000, activation='relu'))
-#model.add(Dense(1000, activation='relu'))
-#model.add(Dense(2048, activation='relu'))
-model.add(Dense(512, activation='relu'))
-model.add(Dense(128, activation='relu'))
+# model.add(Dense(10000, activation='relu'))
+# model.add(Dense(5000, activation='relu'))
+# model.add(Dense(1000, activation='relu'))
+# model.add(Dense(2048, activation='relu'))
+# model.add(Dense(512, activation='relu'))
+# model.add(Dense(128, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(n_classes, activation='sigmoid'))
 model.summary()
@@ -94,40 +94,81 @@ model.layers[0].trainable = False
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['acc'])
-history = model.fit(x_train, y_train,
-                    epochs=10,
-                    batch_size=32,
-                    validation_data=(x_val, y_val))
-model.save_weights('pre_trained_glove_model.h5')
+# history = model.fit(x_train, y_train,
+#                     epochs=10,
+#                     batch_size=32,
+#                     validation_data=(x_val, y_val))
+# model.save_weights('pre_trained_glove_model.h5')
 
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+# acc = history.history['acc']
+# val_acc = history.history['val_acc']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# epochs = range(1, len(acc) + 1)
+#
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation accuracy')
+# plt.legend()
+#
+# plt.figure()
+#
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+#
+# plt.show()
 
-epochs = range(1, len(acc) + 1)
+sentences_test, labels_test, n_ep_test, n_scene_test = load_sentences_persons(EPISODES_TEST)
 
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.legend()
-
-plt.figure()
-
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-
-plt.show()
-
-sentences_test, labels_test = load_sentences_persons(EPISODES_TEST)
+n_ep_test=np.array(n_ep_test).astype(float).astype(int)
+n_scene_test=np.array(n_scene_test).astype(float).astype(int)
 
 sequences_test = tokenizer.texts_to_sequences(sentences_test)
 x_test = pad_sequences(sequences_test, maxlen=maxlen)
 y_test = np.asarray(labels_test)
 y_test = to_categorical(y_test)
+print(x_test.shape, y_test.shape)
 
 model.load_weights('pre_trained_glove_model.h5')
-print(model.evaluate(x_test, y_test))
+#print('TEST ACCURACY ON SENTENCE:', model.evaluate(x_test, y_test)[1])
+threshold_prediction = 0.02
+
+confusion_per_character = np.zeros((n_classes, 2, 2))
+for ep in range(len(n_ep_test)):
+    idx_ep = n_ep_test==ep
+    ep_scene = n_scene_test[idx_ep]
+
+    for sc in list(np.unique(ep_scene)):
+        #print('({}, {})'.format(ep, sc))
+        idx_scene = n_scene_test==sc
+        id_scene = np.logical_and(idx_ep, idx_scene)
+        # print(id_scene)
+        x_scene = x_test[id_scene]
+        y_scene = y_test[id_scene]
+
+        predict_scene_by_sentence = np.array(model.predict(x_scene))
+        predict_scene = np.sum(predict_scene_by_sentence, axis=0)/predict_scene_by_sentence.shape[0]
+        truth_class = np.unique(np.argmax(y_scene, axis=1))
+        predict_class = predict_scene[predict_scene>threshold_prediction].argsort()
+
+        for character in range(n_classes):
+            if character in truth_class and character in predict_class:
+                confusion_per_character[character, 0, 0] += 1
+            elif character in truth_class and character not in predict_class:
+                confusion_per_character[character, 0, 1] += 1
+            elif character not in truth_class and character in predict_class:
+                confusion_per_character[character, 1, 0] += 1
+            elif character not in truth_class and character not in predict_class:
+                confusion_per_character[character, 1, 1] += 1
+
+PERSONSS = PERSONS + ['unknown']
+print('ACCURACY')
+for character in range(n_classes):
+    m_confusion = confusion_per_character[character, :, :]
+    #print(m_confusion)
+    print('{}: {:.4f}\n'.format(PERSONSS[character],
+                              (m_confusion[0,0]+m_confusion[1,1])/m_confusion.sum()))
