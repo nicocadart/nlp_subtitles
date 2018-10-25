08361d8b823c0965c0fc58dbf9447cea79dfb2d4
Cadart Nicolas <nicolas.cadart@ensta-paristech.fr>
2018-10-25 14:15:55 +0200

    [*] New database parsing

--- parsing_toolbox_new.py
+++ parsing_toolbox_new.py
@@ -1,7 +1,5 @@
 import numpy as np
 import csv
-from nltk import sent_tokenize
-from encoding import one_hot_encoding
 
 SEASON = 0
 EPISODE = 1
@@ -11,11 +9,49 @@ PERSON = 4
 SENTENCE = 5
 
 DATABASE_PATH  = 'data/tbbt_db.csv'
-DB_COLUMNS = ['season', 'episode', 'scene', 'scene_id', 'word', 'locutor']
+DB_COLUMNS = ['season', 'episode', 'scene', 'scene_id', 'locutor', 'text']
+DELIMITER = '§'
+
+PERSONS = ['Howard', 'Raj', 'Sheldon', 'Penny', 'Leonard']
 
 
 def load_db():
+    """
+    Load the full database.
+    :return: list of list, where each element has format ['season', 'episode', 'scene', 'scene_id', 'locutor', 'text']
+    """
     with open(DATABASE_PATH, "r", newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter='§')
+        reader = csv.reader(csvfile, delimiter=DELIMITER)
         return list(reader)
+
+
+def get_persons_scenes(db):
+    """
+    Parse database and return persons and text for each scene.
+    :param db: the full database, returned by load_db()
+    :return: persons: list of list, where each element is a person talking during the given scene
+    :return: text: list of strings, where each element is the full text of the given scene
+    """
+    persons = []
+    text = []
+
+    current_scene_id = 'x'
+
+    current_scene_persons = set()
+    current_scene_text = ""
+
+    for db_entry in db:
+
+        if db_entry[SCENE_ID] != current_scene_id:
+            if current_scene_id != "x":
+                persons.append(list(current_scene_persons))
+                text.append(current_scene_text)
+            current_scene_id = db_entry[SCENE_ID]
+            current_scene_persons = set()
+            current_scene_text = ""
+
+        current_scene_persons.add(db_entry[PERSON])
+        current_scene_text += db_entry[SENTENCE]
+
+    return persons, text
 
