0239d07ecaf5cba72420f4d0f2e58ce10e1bd730
Cadart Nicolas <nicolas.cadart@ensta-paristech.fr>
2018-10-25 22:31:53 +0200

    [~] correct one_hot_encoding() and add parameter to disable 'unkwown' state

--- encoding.py
+++ encoding.py
@@ -1,9 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import numpy as np
-from nltk import sent_tokenize
 
-# SEASON EPISODE SCENE SENTENCE WORD TIME DURATION type_EN nom_EN role_EN nom_locuteur
 
 def one_hot_encoding(labels, states, unknown_name='unknown'):
     """@brief: Return one_hot encoding of an array of labels for a given state space of strings
@@ -17,23 +15,20 @@ def one_hot_encoding(labels, states, unknown_name='unknown'):
         one_hot_encode: ndarray, of size (n_samples, n_states + 1)
     """
     # Adding the "unknown" state to the state space
-    states = states + [unknown_name]
-    n_samples = labels.shape[0]
+    states = (states + [unknown_name]) if unknown_name else states
+    n_samples = len(labels)
     n_states = len(states)
 
     one_hot_encode = np.zeros((n_samples, n_states))
 
-    for (i, sample) in enumerate(list(labels)):
-        # Some tags are multiple targets, separated by a +
-        tags_in_sample = sample.split('+')
+    for i, sample in enumerate(labels):
 
         oh = np.zeros((1, n_states))
-        for tag in tags_in_sample:
-            if tag in states:
-                oh[0, states.index(tag)] = 1
+        if sample in states:
+            oh[0, states.index(sample)] = 1
 
         # if target isn't linked to any state, put 1 in "unknown"
-        if np.sum(oh) == 0:
+        if unknown_name:
             oh[0, -1] = 1
 
         one_hot_encode[i, :] = oh
