4ce36b2caffe2bb3d0c752d88dcd6a6d945b7c81
Cadart Nicolas <nicolas.cadart@ensta-paristech.fr>
2018-11-09 18:23:50 +0100

    [*] train_named_entities : update models with gridsearch results, add 2nd layer model

--- train_named_entities.py
+++ train_named_entities.py
@@ -3,6 +3,7 @@ from scipy.optimize import minimize
 import csv
 import warnings
 
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -16,6 +17,9 @@ from parsing_toolbox import PERSONS, UNKNOWN_STATE
 from named_entities_toolbox import get_train_test_ne_persons_dataset
 
 
+NE_MIN_COUNT = 25
+NE_ONCE = False
+POSSIBLE_LOCUTORS = PERSONS + [UNKNOWN_STATE]
 TEST_RESULTS_PATH = "data/prediction_ne_test.csv"
 
 
@@ -33,8 +37,8 @@ def train_models(models, X_train, y_train, X_valid=None, y_valid=None, models_na
 
             if verbose:
                 print('Training model {}'.format(model_name))
-            # models[i_model] = (model_name, CalibratedClassifierCV(models[i_model][1], method='isotonic', cv=3))
-            # model = models[i_model][1]
+            models[i_model] = CalibratedClassifierCV(model, method='isotonic', cv=3)
+            model = models[i_model]
             model.fit(X_train, y_train)
 
             if verbose and X_valid.any() and y_valid.any():
@@ -47,8 +51,12 @@ def models_predict_proba(models, X):
     y_proba_pred = []
     for model in models:
         y_proba_pred.append(model.predict_proba(X))
-    return models
+    return y_proba_pred
 
+
+def models_predict_proba_satcked(models, X):
+    y_pred_p_models = models_predict_proba(models, X)
+    return np.hstack(tuple(y_pred_p[:,:-1] for y_pred_p in y_pred_p_models))
 
 def modelmix_predict_proba(models, weights, X):
     """
@@ -63,7 +71,7 @@ def modelmix_predict_proba(models, weights, X):
     @return:
             y_pred_p: ndarray, (n_samples, n_classes), probability for each class for each sample
     """
-    n_classes = models[-1].n_classes_
+    n_classes = len(models[0].classes_)
     y_pred_p = np.zeros((X.shape[0], n_classes))
     for i_model, model in enumerate(models):
         y_pred_p += weights[i_model] * model.predict_proba(X)
@@ -71,33 +79,24 @@ def modelmix_predict_proba(models, weights, X):
 
 
 def train_model_mix(models, X, y, score='logloss'):
-    """
-    @brief: Load gridsearch result in .csv file
-    @param:
-            models: list of sklearn models (with params already passed) as a
-                    list of tuple (name, model)
+    """ Compute best weights for ensemble learning """
+    n_classes = len(models[0].classes_)
 
-            X_train: ndarray (n_samples, n_features), array of samples to train
-            y_train: ndarray (n_samples,), array of targets for each train sample
-
-            X_val: ndarray (n_samples, n_features), array of samples to test
-            y_val: ndarray (n_samples,), array of targets for each test sample
-
-    @return:
-            (print confusion matrix and log loss score for the final model (weighted prediction))
-            models: list of learned sklearn models as a list of tuple (name, model)
-            optimal_weights: weights for ponderation between model prediction,
-                             optimized for those models
-     """
     # predict proba
     y_proba_pred = models_predict_proba(models, X)
 
     def log_loss_func(weights):
-        y_pred_mix = modelmix_predict_proba(models, weights, X)
+        y_pred_mix = np.zeros((X.shape[0], n_classes))
+        for i_model, model in enumerate(models):
+            y_pred_mix += weights[i_model] * y_proba_pred[i_model]
+        # y_pred_mix = modelmix_predict_proba(models, weights, X)
         return log_loss(y, y_pred_mix)
 
     def error_rate_func(weights):
-        y_pred_mix = modelmix_predict_proba(models, weights, X)
+        y_pred_mix = np.zeros((X.shape[0], n_classes))
+        for i_model, model in enumerate(models):
+            y_pred_mix += weights[i_model] * y_proba_pred[i_model]
+        # y_pred_mix = modelmix_predict_proba(models, weights, X)
         return 1 - accuracy_score(y, np.argmax(y_pred_mix, axis=1))
 
     # function we want to minimize
@@ -124,10 +123,7 @@ if __name__=="__main__":
     #                         LOAD DATASET
     # =================================================================
 
-    min_count = 10
-    once = False
-    possible_locutors = PERSONS + [UNKNOWN_STATE]
-    ne_dataset = get_train_test_ne_persons_dataset(possible_locutors, ne_min_count=min_count, once=once)
+    ne_dataset = get_train_test_ne_persons_dataset(POSSIBLE_LOCUTORS, ne_min_count=NE_MIN_COUNT, once=NE_ONCE)
     X_train, y_train, ids_train, X_valid, y_valid, ids_valid, X_test, y_test, ids_test = ne_dataset
 
     print("Dimensions of datasets :")
@@ -139,10 +135,10 @@ if __name__=="__main__":
     #                      TRAIN CLASSIFIERS
     # =================================================================
 
-    test_results = np.zeros((X_test.shape[0], 2*len(possible_locutors)))
+    test_results = np.zeros((X_test.shape[0], 2*len(POSSIBLE_LOCUTORS)))
 
     # Train models for each person and print results
-    for i_person, person in enumerate(possible_locutors):
+    for i_person, person in enumerate(POSSIBLE_LOCUTORS):
         print("\n==================================================")
         print("{:^50}".format(person.upper()))
         print("==================================================")
@@ -164,39 +160,71 @@ if __name__=="__main__":
         # clfs.append(SVC(kernel='rbf', C=1, probability=True))
 
         clfs_names.append('LogisticRegression')
-        parameters = {}
+        parameters = {'penalty': 'l2',
+                      'C': 0.1}
         clfs.append(LogisticRegression(**parameters))
 
-        clfs_names.append('DecisionTreeClassifier 1')
-        parameters = {'max_depth':None,
-                      'min_samples_split':3}
-        clfs.append(DecisionTreeClassifier(**parameters))
-
-        clfs_names.append('RandomForestClassifier 1')
-        parameters = {'n_estimators': 50}
-        clfs.append(RandomForestClassifier(**parameters))
+        # clfs_names.append('RandomForestClassifier 1')
+        # parameters = {'min_samples_split': 2,
+        #               'n_estimators': 200,
+        #               'max_depth': 10,
+        #               'criterion': 'entropy',
+        #               'n_jobs': -1}
+        # clfs.append(RandomForestClassifier(**parameters))
 
         clfs_names.append('RandomForestClassifier 2')
-        parameters = {'n_estimators': 100}
+        parameters = {'min_samples_split': 4,
+                      'n_estimators': 100,
+                      'max_depth': 20,
+                      'criterion': 'gini',
+                      'n_jobs': -1}
         clfs.append(RandomForestClassifier(**parameters))
 
-        clfs_names.append('MLPClassifier')
-        parameters = {'hidden_layer_sizes': (50, 25, 5),
-                      'activation':'logistic',
-                      'early_stopping': True}
-        clfs.append(MLPClassifier(**parameters))
+        # clfs_names.append('MLPClassifier 1')
+        # parameters = {'activation': 'relu',
+        #               'early_stopping': True,
+        #               'hidden_layer_sizes': (90,),
+        #               'batch_size': 128,
+        #               'alpha': 0.0001}
+        # clfs.append(MLPClassifier(**parameters))
+        #
+        # clfs_names.append('MLPClassifier 2')
+        # parameters = {'activation': 'relu',
+        #               'early_stopping': True,
+        #               'hidden_layer_sizes': (70, 50, 25, 15),
+        #               'batch_size': 128,
+        #               'alpha': 0.001}
+        # clfs.append(MLPClassifier(**parameters))
+        #
+        # clfs_names.append('SVM')
+        # parameters = {'probability': True,
+        #               'kernel': 'rbf',
+        #               'C': 10.0}
+        # clfs.append(SVC(**parameters))
 
-        clfs_names.append('XGBoost')
-        parameters = {'objective': 'binary:logistic',
-                      'n_estimators': 150,
-                      'max_depth': 9,
-                      'learning_rate': 0.1,
-                      'subsample': 0.7,
+        clfs_names.append('XGBoost 1')
+        parameters = {'n_estimators': 100,
+                      'objective': 'binary:logistic',
                       'colsample_bytree': 0.8,
-                      'reg_lambda': 0,
-                      'reg_alpha': 1,
-                      'n_jobs': -1}
+                      'subsample': 1,
+                      'max_depth': 9,
+                      'reg_lambda': 1,
+                      'n_jobs': -1,
+                      'learning_rate': 0.1,
+                      'reg_alpha': 0}
         clfs.append(XGBClassifier(**parameters))
+
+        # clfs_names.append('XGBoost 2')
+        # parameters = {'n_estimators': 200,
+        #               'objective': 'binary:logistic',
+        #               'colsample_bytree': 0.8,
+        #               'subsample': 1,
+        #               'max_depth': 9,
+        #               'reg_lambda': 1,
+        #               'n_jobs': -1,
+        #               'learning_rate': 0.1,
+        #               'reg_alpha': 1}
+        # clfs.append(XGBClassifier(**parameters))
 
         # -------------------
         #  Train classifiers
@@ -212,18 +240,56 @@ if __name__=="__main__":
 
         # get optimal weights
         best_weights = train_model_mix(clfs, X_valid, y_valid[person], score='logloss')
-        y_pred_proba = modelmix_predict_proba(clfs, best_weights, X_valid)
+        y_pred_proba = modelmix_predict_proba(clfs, best_weights, X_test)
         y_pred = np.argmax(y_pred_proba, axis=1)
 
         # print results
-        print(" * Best Weights   : {}".format(np.round(best_weights, 3)))
-        print(" * Final accuracy : {:.2f}%".format(100*accuracy_score(y_valid[person], y_pred)))
-        print(" * Final log-loss : {:.3f}".format(log_loss(y_valid[person], y_pred_proba)))
+        print(" * Best Weights  : {}".format(np.round(best_weights, 3)))
+        print(" * Test accuracy : {:.2f}%".format(100*accuracy_score(y_test[person], y_pred)))
+        print(" * Test log-loss : {:.3f}".format(log_loss(y_test[person], y_pred_proba)))
         print("{}".format(confusion_matrix(y_test[person], y_pred)))
 
         # save test results
         test_results[:, i_person] = y_test[person]
-        test_results[:, len(possible_locutors)+i_person] = y_pred_proba[:, 1]
+        test_results[:, len(POSSIBLE_LOCUTORS)+i_person] = y_pred_proba[:, 1]
+
+        # -------------------------
+        #  Second layer classifier
+        # -------------------------
+        print("\nSecond layer of classifier")
+
+        # concatenate predictions from 1st layer classifiers
+        X_train2 = models_predict_proba_satcked(clfs, X_train)
+        X_valid2 = models_predict_proba_satcked(clfs, X_valid)
+        X_test2 = models_predict_proba_satcked(clfs, X_test)
+
+        # classifier
+        # parameters = {'objective': 'binary:logistic',
+        #               'n_estimators': 200,
+        #               'max_depth': 9,
+        #               'learning_rate': 0.1,
+        #               'subsample': 0.7,
+        #               'colsample_bytree': 0.8,
+        #               'reg_lambda': 1,
+        #               'reg_alpha': 0,
+        #               'n_jobs': -1}
+        # final_model = XGBClassifier(**parameters)
+
+        parameters = {'n_estimators': 200,
+                      'max_depth': 9,
+                      'min_samples_split': 2}
+        final_model = RandomForestClassifier(**parameters)
+
+        # train on dataset and test predictions
+        final_model.fit(X_train2, y_train[person])
+        y_pred_proba = final_model.predict_proba(X_test2)
+        y_pred = np.argmax(y_pred_proba, axis=1)
+
+        # print results
+        print(" * Test accuracy : {:.2f}%".format(100*accuracy_score(y_test[person], y_pred)))
+        print(" * Test log-loss : {:.3f}".format(log_loss(y_test[person], y_pred_proba)))
+        print("{}".format(confusion_matrix(y_test[person], y_pred)))
+
 
     # save results in csv
     with open(TEST_RESULTS_PATH, "w", newline='') as csvfile:
